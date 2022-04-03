fa2463a41e617a1ca09bfa73874375c243e0e018
Cundrat Juninho Kuth <87369621+Cundrat@users.noreply.github.com>
2022-04-03 19:11:44 +0700

    Create Score Convert.py

---
+++ English-Version/python-projects/py4e/Score Convert.py
@@ -0,0 +1,14 @@
+score = input("Enter score: ")
+fs = float(score)
+if fs >= 1.1:
+    print("Error")
+elif fs >= 0.9:
+    print("A")
+elif fs >= 0.8:
+    print("B")
+elif fs >= 0.7:
+    print("C")
+elif fs >= 0.6:
+    print("D")
+elif fs < 0.6:
+    print("F")
