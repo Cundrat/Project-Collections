9b35620533a16b185871deec25b30fe80cf95b5a
Cundrat Juninho Kuth <87369621+Cundrat@users.noreply.github.com>
2022-04-04 10:07:09 +0700

    Update Salary Calculation.py

--- English-Version/python-projects/py4e/Salary Calculation.py
+++ English-Version/python-projects/py4e/Salary Calculation.py
@@ -1,21 +1,16 @@
-# eh = enter hours
-# er = enter rate
-# ot = overtime
-# rg = regular pay
-
+def computepay(hours, rate):
+    if hours > 40:
+        regular = hours * rate
+        overtime = (hours - 40) * (rate * 0.5)
+        pay = regular + overtime
+    else:
+        pay = hours * rate
+    return pay
 
 eh = input("Enter Hours: ")
 er = input("Enter Rate: ")
 fh = float(eh)
 fr = float(er)
 
-if fh > 40:
-    #overtime
-    rg = fh * fr
-    ot = (fh - 40) * (fr * 0.5)
-    ro = rg + ot
-else:
-    #regular
-    ro = fh * fr
-
-print("Pay:", ro)
+ro = computepay(fh, fr)
+print("Pay", ro)
