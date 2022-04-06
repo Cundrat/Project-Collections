5c7fd11703de45d1e911929b788693c9518c96a0
Cundrat Juninho Kuth <87369621+Cundrat@users.noreply.github.com>
2022-04-06 17:48:24 +0700

    Create counting, summing, averaging in a loop.py

---
+++ English-Version/python-projects/FreeCodeCamp/counting, summing, averaging in a loop.py
@@ -0,0 +1,9 @@
+count = 0
+sum_of_list = 0
+for i in [9, 41, 12, 3, 74, 15]:
+    count += 1
+    sum_of_list += i
+    average = sum_of_list / count
+print('Count of list:', count)
+print('Sum of list:', sum_of_list)
+print('Average of list:', average)
