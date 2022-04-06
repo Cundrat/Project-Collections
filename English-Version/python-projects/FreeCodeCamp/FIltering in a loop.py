3e1d68d6f4ef05876d6d7dd12a2f62b98adaa392
Cundrat Juninho Kuth <87369621+Cundrat@users.noreply.github.com>
2022-04-06 18:16:59 +0700

    Update FIltering in a loop.py

--- English-Version/python-projects/FreeCodeCamp/FIltering in a loop.py
+++ English-Version/python-projects/FreeCodeCamp/FIltering in a loop.py
@@ -6,4 +6,8 @@ for i in [9, 41, 12, 3, 74, 15]:
     if i > 20:
         print('Greater than 20 is', i)
         a += 1
-print('There are', a,'Values that greater than 20')
+        if a > 1:
+            t = 'are'
+        else:
+            t = 'is'
+print('There',t, a,'values that greater than 20')
