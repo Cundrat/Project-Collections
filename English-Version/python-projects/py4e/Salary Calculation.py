d16ab7f88b91fab5f6ad2c1fac2f5dc5da28065d
Cundrat Juninho Kuth <87369621+Cundrat@users.noreply.github.com>
2022-04-06 14:24:03 +0700

    Update Salary Calculation.py

--- English-Version/python-projects/py4e/Salary Calculation.py
+++ English-Version/python-projects/py4e/Salary Calculation.py
@@ -1,16 +1,23 @@
+#This project is belongs to py4e, this is just that exercise or project that i've worked
+#This program write to prompt prompt the user for hours and rate per hour using input to compute gross pay with own function.
+
+
 def computepay(hours, rate):
     if hours > 40:
         regular = hours * rate
-        overtime = (hours - 40) * (rate * 0.5)
+        overtime = (hours - 40) * (rate *0.5)
         pay = regular + overtime
     else:
         pay = hours * rate
     return pay
 
-eh = input("Enter Hours: ")
-er = input("Enter Rate: ")
-fh = float(eh)
-fr = float(er)
 
-ro = computepay(fh, fr)
-print("Pay", ro)
+
+hours = input("Enter Hours:")
+rate  = input("Enter Rate:")
+hours = float(hours)
+rate  = float(rate)
+
+
+pay = computepay(hours,rate)
+print("Pay", pay)
